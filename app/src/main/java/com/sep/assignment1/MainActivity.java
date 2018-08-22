5a0c86cb11f27f747745fc8ea319f3d593368bb7
darrenhk2010 <darrenhk2010@outlook.com>
2018-08-22 16:36:15 +1000

    Revert "Test"
    This reverts commit 756e278e13866a9b922b3789b3fbb80da22a2c3f.

--- app/src/main/java/com/sep/assignment1/MainActivity.java
+++ app/src/main/java/com/sep/assignment1/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 //test
-    //Hello World
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
