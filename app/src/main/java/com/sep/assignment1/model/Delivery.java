5a878a22fd5dc908d06a5335ca3536af4eba5b4d
rnaahs <rnaahs@gmail.com>
2018-09-02 19:48:10 +1000

    Delivery commit

---
+++ app/src/main/java/com/sep/assignment1/model/Delivery.java
@@ -0,0 +1,70 @@
+package com.sep.assignment1.model;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class Delivery extends AppCompatActivity {
+
+    private String driverId;
+    private String customerId;
+    private String resturantId;
+    private String userName;
+    private int password;
+    private String email;
+    private String status;
+
+    public String getDriverId() {
+        return driverId;
+    }
+
+    public void setDriverId(String driverId) {
+        this.driverId = driverId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getResturantId() {
+        return resturantId;
+    }
+
+    public void setResturantId(String resturantId) {
+        this.resturantId = resturantId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
